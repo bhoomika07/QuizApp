2d9a772f0f8af30a5a078ae75d2bc21102c0375b
Bhoomika Valani <bhoomikavalani07@gmail.com>
2021-04-24 19:14:16 +0530

    added quiz and results

---
+++ js/test.js
@@ -0,0 +1,7 @@
+var submit_btn=document.getElementById('btn');
+submit_btn.onclick=()=>{
+    var x=document.getElementById('quiz');
+    x.style.display="none";
+    var y=document.getElementById('res');
+    y.style.display="block";
+}
