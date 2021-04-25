49522e48ff6befb2d5535b26a05802ca6c3d8ddc
Bhoomika Valani <bhoomikavalani07@gmail.com>
2021-04-25 22:33:28 +0530

    student records added

--- js/admin_dash.js
+++ js/admin_dash.js
@@ -4,6 +4,8 @@ create_btn.onclick = () =>{
     disp.style.display="block";
     var records=document.getElementById('rec');
     records.style.display="none";
+    var dashboard=document.getElementById('dashboard');
+    dashboard.style.display="none";
 }
 
 var rec_btn=document.getElementById('show');
@@ -12,4 +14,15 @@ rec_btn.onclick = () =>{
     show.style.display="block";
     var ed=document.getElementById('edit');
     ed.style.display="none";
+    var dashboard=document.getElementById('dashboard');
+    dashboard.style.display="none";
+}
+var dash_btn=document.getElementById('dash_btn');
+dash_btn.onclick = () =>{
+    var dashboard=document.getElementById('dashboard');
+    dashboard.style.display="block";
+    var ed=document.getElementById('edit');
+    ed.style.display="none";
+    var records=document.getElementById('rec');
+    records.style.display="none";
 }
