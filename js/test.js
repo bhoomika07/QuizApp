f20ad82a51de2980e3cf38cabe90c01b585c5488
Bhoomika Valani <bhoomikavalani07@gmail.com>
2021-04-25 19:41:39 +0530

    clean up

--- js/test.js
+++
@@ -1,7 +0,0 @@
-var submit_btn=document.getElementById('btn');
-submit_btn.onclick=()=>{
-    var x=document.getElementById('quiz');
-    x.style.display="none";
-    var y=document.getElementById('res');
-    y.style.display="block";
-}
